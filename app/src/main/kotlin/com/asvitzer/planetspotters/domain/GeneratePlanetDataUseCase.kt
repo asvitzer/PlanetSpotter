b0b88fb56a8f892b745bc22ee8ca5ae4c9f3a286
Alvin Svitzer <asvitzer@gmail.com>
2024-08-12 12:55:11 -0400

    Updating String

--- app/src/main/kotlin/com/asvitzer/planetspotters/domain/GeneratePlanetDataUseCase.kt
+++ app/src/main/kotlin/com/asvitzer/planetspotters/domain/GeneratePlanetDataUseCase.kt
@@ -3,13 +3,15 @@ package com.asvitzer.planetspotters.domain
 import com.asvitzer.planetspotters.data.WorkResult
 import com.asvitzer.planetspotters.data.repo.GeminiRepository
 import kotlinx.coroutines.flow.Flow
+import java.util.Stack
 import javax.inject.Inject
 
 class GeneratePlanetDataUseCase @Inject constructor(
     private val geminiRepository: GeminiRepository
 ) {
     companion object {
-        const val FUN_FACTS_PROMPT: String = "Give me some fun facts, no more than 3, for this planet. If there are no fun facts, please say that there are none and give the reason why:"
+        const val FUN_FACTS_PROMPT: String =
+            "Give me some fun facts, no more than 3, for this planet. If there are no fun facts, please say that there are none and give the reason why:"
     }
 
     suspend operator fun invoke(prompt: String): Flow<WorkResult<String?>> {
